e2965b2b6f588136b0771b1293420e35df2ca85d
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-08 13:30:33 +0530

    Created two classes public class Car that has getter and setter methods for all the variables. public class Main that has obj of Car class

--- src/Car.java
+++ src/Car.java
@@ -13,11 +13,47 @@ public class Car {
     private String color;
 
     public void setModel(String model) {
-        this.model = model;
+        String validModel = model.toLowerCase();
+        if((validModel.equals("carrera")) || (validModel.equals("suzuki"))) {
+            this.model = model;
+        } else  {
+            this.model = "Unknown";
+        }
     }
-
     public String getModel() {
         return this.model;
     }
+
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+    public int getDoors() {
+        return this.doors;
+    }
+
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+    public int getWheels() {
+        return  this.wheels;
+    }
+
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+    public String getEngine() {
+        return this.engine;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return this.color;
+    }
+
 }
 
--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,23 @@ public class Main {
     public static void main(String[] args) {
         Car obj = new Car();
         Car obj1 = new Car();
+
         obj.setModel("Carrera");
         System.out.println("Model is: " + obj.getModel());
+        obj1.setModel("119");
+        System.out.println("Model is: " + obj1.getModel());
+
+        obj.setColor("Balck");
+        System.out.println("Color is: " + obj.getColor());
+
+        obj.setDoors(4);
+        System.out.println("Doors are: " + obj.getDoors());
+
+        obj.setEngine("abc");
+        System.out.println("Engine is: " + obj.getEngine());
+
+        obj.setWheels(4);
+        System.out.println("Wheels are: " + obj.getWheels());
     }
 }
 
