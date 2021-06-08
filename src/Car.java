48c79f6e25ae58f334dab61a1b900a9bca3d243a
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-08 13:18:38 +0530

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Class And Object
+ * Package Name: PACKAGE_NAME
+ * Date: 08-06-2021
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Class And Object
+ * Package Name: PACKAGE_NAME
+ * Date: 08-06-2021
+ */
+public class Main {
+    public static void main(String[] args) {
+        Car obj = new Car();
+        Car obj1 = new Car();
+        obj.setModel("Carrera");
+        System.out.println("Model is: " + obj.getModel());
+    }
+}
+
